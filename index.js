d6264940cdb7c6c10b2f38f073203448ca1a736b
Nick <naaltunian@icloud.com>
2020-02-17 13:21:00 -0500

    loop over provided urls and save pdf of homepage

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const puppeteer = require("puppeteer");
 
-// add whichever sites you'd like to save here. You'll also need to create subdirectories within the newspapers directory with the name of the site, ex: https://nytimes.com -> nytimes
-const urls = ["https://nytimes.com", "https://www.vox.com", "https://arstechnica.com", "https://www.washingtonpost.com"];
+// add whichever sites you'd like to save here with the prefix www. in order for the split to work correctly. You'll also need to create subdirectories within the newspapers directory with the name of the site, ex: https://nytimes.com -> nytimes.com, https://www.washingtonpost.com -> washingtonpost.com
+const urls = ["https://www.nytimes.com", "https://www.vox.com", "https://www.arstechnica.com", "https://www.washingtonpost.com"];
 
 const todaysDate = new Date().toLocaleDateString().replace(/\//g, "-");
 
-const path = `${__dirname}/newspapers/nytimes/${todaysDate}.pdf`;
-console.log(path)
-
-
-const nytUrl = "https://nytimes.com"
 async function run() {
     const browser = await puppeteer.launch();
+
+    for(let url of urls) {
+        let site = url.split("//www.")[1];
+        let path = `${__dirname}/newspapers/${site}/${todaysDate}.pdf`
+        await savePDF(browser, url, path)
+    }
+    await browser.close();
+}
+
+async function savePDF(browser, url, path) {
     const page = await browser.newPage();
-    await page.goto(nytUrl);
+    await page.goto(url, {
+        waitUntil: 'load',
+        timeout: 0
+    });
     await page.pdf({
         path,
         format: "A4"
     });
-    await browser.close();
 }
 
 run();

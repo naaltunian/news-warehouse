8b5ec8974ca7626c50eb8223b3f67a57c43ca0d0
Nick <naaltunian@icloud.com>
2020-02-17 14:31:56 -0500

    Add setup script

---
+++ setup.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const { urls } = require("./index");
+
+function createDirectories() {
+    for(let directory of urls) {
+        let site = directory.split("//www.")[1];
+        let path = `${__dirname}/newspapers/${site}`;
+        if(!fs.existsSync(path)) {
+            fs.mkdirSync(path);
+        }
+    }
+    process.exit();
+}
+
+createDirectories();
